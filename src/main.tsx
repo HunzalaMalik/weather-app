cf6d8ffa9c9b740db52ca6c8609f113f69a758b3
Hunzala Malik <hunzala.malik@devsinc.com>
2023-08-24 05:38:18 -0700

    Add basic config/dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,12 @@ import { Provider } from "react-redux"
 import { store } from "./app/store"
 import App from "./App"
 import "./index.css"
+import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
